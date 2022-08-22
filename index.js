8058407c77e036cf7423f2f73342e421ff7860c8
mehLabs <hugo.iturrieta@protonmail.com>
2022-08-22 08:50:52 -0300

    v0.1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const app = express();
+const port = 3000;
+const db = require('./queries');
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended:true,
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({info:'API hecho con postgreSQL, Express y Node.js'})
+}) 
+
+
+app.get('/usuarios',db.getUsers);
+app.post('/usuarios',db.createUser);
+app.delete('/usuarios/:id',db.deleteUserById);
+app.get('/usuarios/:id',db.getUserById);
+app.put('/usuarios/:id',db.updateUser);
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto: ${port}`);
+})
---
+++ queries.js
@@ -0,0 +1,68 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'me',
+    host: 'localhost',
+    database: 'api',
+    password: 'sove2012',
+    port: 5432,
+})
+
+const getUsers = (request,response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error,results) => {
+        if (error){
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getUserById = (request,response) => {
+    pool.query('SELECT * FROM users WHERE id = $1',[id],(error,results) => {
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const createUser = (request,response) => {
+    const {name, email, password} = request.body;
+
+    pool.query('INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *', [name,email,password], (error,results) => {
+        if (error){
+            throw error;
+        }
+        response.status(201).send(`Usuario añadido con ID: ${results.rows[0].id}`)
+    })
+}
+
+const updateUser = (request,response) => {
+    const id = parseInt(request.params.id);
+    const {name,email,password} = request.body;
+
+    pool.query('UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4',[name,email,password,id],(error,results) => {
+        if (error){
+            throw error
+        }
+        response.status(200).send(`Usuario con ID: ${id} modificado`)
+    })
+}
+
+const deleteUserById = (request,response) => {
+    const id = parseInt(request.params.id);
+    
+    pool.query('DELETE FROM users WHERE id = $1',[id],(error,results)=>{
+        if (error){
+            throw error
+        }
+        response.status(200).send(`Usuario con ID: ${id} eliminado correctamente.`)
+    })
+}
+
+module.exports = {
+    getUsers,
+    createUser,
+    deleteUserById,
+    getUserById,
+    updateUser
+}
